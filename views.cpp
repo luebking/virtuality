22ba42355713af12bfdce99832ad1221d4abde00
Thomas Lübking <thomas.luebking@gmail.com>
2014-10-28 23:04:03 +0100

    only take a truly transparent color as round calendar item indicator; cause of QSS

--- views.cpp
+++ views.cpp
@@ -463,15 +463,17 @@ Style::drawItem(const QStyleOption *option, QPainter *painter, const QWidget *wi
             painter->setPen(Qt::NoPen);
             if (selected && item->viewItemPosition == QStyleOptionViewItemV4::Invalid && widget &&
                 widget->inherits("QCalendarView")) {
-                SAVE_PAINTER(Alias);
-                const int s = qMin(RECT.width(), RECT.height());
-                QRect r(0,0,s,s);
-                r.moveCenter(RECT.center());
-                painter->setRenderHint(QPainter::Antialiasing);
-                high.setAlpha(255);
-                painter->setBrush(high);
-                painter->drawEllipse(r);
-                RESTORE_PAINTER
+                if (!high.alpha()) { // this is the color we cheated to transparent in polish.cpp
+                    SAVE_PAINTER(Alias);
+                    const int s = qMin(RECT.width(), RECT.height());
+                    QRect r(0,0,s,s);
+                    r.moveCenter(RECT.center());
+                    painter->setRenderHint(QPainter::Antialiasing);
+                    high.setAlpha(255);
+                    painter->setBrush(high);
+                    painter->drawEllipse(r);
+                    RESTORE_PAINTER
+                }
             } else {
                 painter->setBrush(high);
                 painter->drawRect(RECT);
